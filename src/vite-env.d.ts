3f9c8625a01256674e350d99e36eaec7fd47d5bf
Kauan Lopes <105743900+KauanLopez@users.noreply.github.com>
2025-06-16 14:16:54 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+// Declara o objeto global 'firebase' para que o TypeScript
+// o reconheça em toda a aplicação, já que ele é carregado
+// pelos scripts no arquivo index.html.
+declare var firebase: any;
